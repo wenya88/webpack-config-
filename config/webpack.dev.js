f50931024839dc26154fecaf39602ed1fac974d4
zhangWY <63963999@qq.com>
2018-07-18 22:27:16 +0800

    webpack.dev.js

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,12 +8,12 @@ module.exports={
 	mode:"development",
 	//出口：有且只能有一个
 	output:{
-		filename:"[name]-bundle.js",
-		path:path.resolve(__dirname,"../dist"),
-		publicPath:'/'
+		filename:"[name]-bundle.js",//输出文件名 [name] 是入口main的文件名 会生成main-bundle.js
+		path:path.resolve(__dirname,"../dist"), //输入文件路径
+		publicPath:'/'//虚拟路径,如果设置publicPath:'/js' 那在index.html <script src="/js/main-bundle.js"></script>
 	},
 	//本地服务器
 	devServer:{
-		contentBase:"dist"
+		contentBase:"dist"//运行那个文件夹下面的文件
 	}
 }
