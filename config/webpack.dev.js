520a2b3de5fcbd2bd2f6280ab2f87e89483d2bc5
zhangWY <63963999@qq.com>
2018-07-18 22:09:52 +0800

    webapck

---
+++ config/webpack.dev.js
@@ -0,0 +1,19 @@
+const path=require('path');
+module.exports={
+	//入口：有并且可以有多个
+	entry:{ 
+		main:"./src/main.js"
+	},
+	//打包环境：开发&生产
+	mode:"development",
+	//出口：有且只能有一个
+	output:{
+		filename:"[name]-bundle.js",
+		path:path.resolve(__dirname,"../dist"),
+		publicPath:'/'
+	},
+	//本地服务器
+	devServer:{
+		contentBase:"dist"
+	}
+}
