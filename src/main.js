c3b5ecd6b861e41d05ab282f073da2a952eac41e
zhangWY <63963999@qq.com>
2018-11-08 21:05:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 require('babel-runtime/regenerator');//识别es8
 require('./main.css');
 require('./index.html');
+
+const prom=new Promise((resolve,reject)=>{
+    let mRandom=Math.random();
+    if(mRandom<0.5){
+        resolve(mRandom);
+    }else{
+        reject(mRandom)
+    }
+});
+async function fun(){
+    let pro=await prom;
+    return pro
+}
+fun().then(val=>{
+    console.log('resolve:'+val)
+}).catch(val=>{
+    console.log('reject:'+val)
+})
